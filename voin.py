bd9b01ffffa94249d19332ab71e2544f34a83551
Kirieshka777 <107071489+Kirieshka777@users.noreply.github.com>
2023-05-11 20:46:07 +0300

    доделано

--- voin.py
+++ voin.py
@@ -12,15 +12,16 @@ class Voin:
         print(f'{pl2.name} атаковал {pl1.name} и нанёс ему 20 ур! У {pl1.name} осталось {pl1.hp}')
 pl1 = Voin('1')
 pl2 = Voin('2')
-randatt = random.randint(1,2)
 while pl1.hp != 0 or pl2.hp != 0:
+    if input() == 'e':
+        randatt = random.randint(1,2)
     if randatt == 1:
         pl1.attackpl1()
     if randatt == 2:
         pl2.attackpl2()
-if pl1.hp == 0:
-    print(f'{pl2.name} победил!!!')
-if pl2.hp == 0:
-    print(f'{pl1.name} победил!!!')
+    if pl1.hp == 0:
+        print(f'{pl2.name} победил!!!')
+    if pl2.hp == 0:
+        print(f'{pl1.name} победил!!!')
     
     
